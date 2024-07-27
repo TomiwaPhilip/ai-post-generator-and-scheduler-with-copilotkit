063de1621bc1a56315dce3d74244ff5c4b719531
Tomiwa Philip <110630370+TomiwaPhilip@users.noreply.github.com>
2024-07-27 06:56:16 +0000

    fixed db connection

--- src/app/api/schedule/route.ts
+++ src/app/api/schedule/route.ts
@@ -3,23 +3,12 @@ import { scheduleJobs } from "@/worker";
 import cron from "node-cron";
 import mongoose from 'mongoose';
 import Schedule from './models/Schedule';
-
-const MONGODB_URI = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) {
-    throw new Error("MONGODB_URI not provided")
-}
-
-// Connect to MongoDB
-mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 5000
-});
+import connectToDB from "@/app/util";
 
 export async function POST(req: NextRequest) {
     const { schedule } = await req.json();
     try {
+        await connectToDB();
         const newSchedule = new Schedule({ schedule });
         await newSchedule.save();
 
@@ -42,6 +31,8 @@ export async function POST(req: NextRequest) {
 
 export async function GET() {
     try {
+        await connectToDB();
+
         const latestSchedule = await Schedule.findOne().sort({ createdAt: -1 });
         if (latestSchedule) {
             return NextResponse.json(
